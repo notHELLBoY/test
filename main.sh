d401bbbcf89f9df69f40eea98d94e8d6c1d6aa02
notHELLBoY <ghostxhellboy@gmail.com>
2024-09-13 21:06:16 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,34 +5,26 @@ generate_ipv4() {
     echo "$((RANDOM % 256)).$((RANDOM % 256)).$((RANDOM % 256)).$((RANDOM % 256))"
 }
 
-# استفاده از API رایگان برای پیدا کردن کشور مرتبط با آدرس IPv4
-get_country() {
-    local ip=$1
-    country=$(curl -s https://ipinfo.io/$ip/country)
-    
-    # چک کردن اینکه آیا درخواست به درستی انجام شده یا نه
-    if [ -z "$country" ]; then
-        echo "Country information not available for IP: $ip"
-    else
-        echo "Country: $country"
-    fi
-}
-
 # پینگ گرفتن از آدرس IP
 ping_ip() {
     local ip=$1
     echo "Pinging IP: $ip"
+    ping -c 1 -W 1 $ip &> /dev/null
     
-    # تست پینگ با استفاده از 4 بسته
-    if ping -c 4 $ip > /dev/null; then
+    if [ $? -eq 0 ]; then
         echo "Ping successful!"
     else
-        echo "Ping failed."
+        echo "Ping failed!"
     fi
 }
 
+# تعداد آدرس‌های IP که می‌خواهیم اسکن کنیم
+num_ips=20
+
 # اجرای اسکریپت
-ip=$(generate_ipv4)
-echo "Generated IP: $ip"
-get_country $ip
-ping_ip $ip
+for ((i=1; i<=num_ips; i++)); do
+    ip=$(generate_ipv4)
+    echo "Generated IP: $ip"
+    ping_ip $ip
+    echo "-----------------------"
+done
